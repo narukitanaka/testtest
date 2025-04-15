c6a1d49e24082c474d48d75ca05a674113d71806
田中成来 <tanaka@g-hill.jp>
2025-04-15 18:42:01 +0900

    初回コミット

---
+++ mail.php
@@ -0,0 +1,60 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  // フォームからのデータを取得
+  $name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
+  $company = htmlspecialchars($_POST['company'], ENT_QUOTES, 'UTF-8');
+  $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
+  $phone = htmlspecialchars($_POST['phone'], ENT_QUOTES, 'UTF-8');
+  $message = htmlspecialchars($_POST['message'], ENT_QUOTES, 'UTF-8');
+
+  // 送信先メールアドレス
+  $to = "register@g-hill.jp";
+
+  // メールの件名
+  $subject = "お問い合わせがありました";
+
+  // メール本文
+  $body = "以下の内容でお問い合わせがありました。\n\n";
+  $body .= "お名前: $name\n";
+  $body .= "会社名: $company\n";
+  $body .= "メールアドレス: $email\n";
+  $body .= "電話番号: $phone\n";
+  $body .= "お問い合わせ内容:\n$message\n";
+
+  // メールヘッダー
+  $headers = "From: $email\r\n";
+  $headers .= "Reply-To: $email\r\n";
+
+  // メール送信
+  if (mail($to, $subject, $body, $headers)) {
+    // 自動返信メールの件名
+    $autoReplySubject = "お問い合わせありがとうございます";
+
+    // 自動返信メール本文
+    $autoReplyBody = "$name 様\n\n";
+    $autoReplyBody .= "この度はお問い合わせいただきありがとうございます。\n";
+    $autoReplyBody .= "以下の内容でお問い合わせを受け付けました。\n\n";
+    $autoReplyBody .= "----------------------------\n";
+    $autoReplyBody .= "お名前: $name\n";
+    $autoReplyBody .= "会社名: $company\n";
+    $autoReplyBody .= "メールアドレス: $email\n";
+    $autoReplyBody .= "電話番号: $phone\n";
+    $autoReplyBody .= "お問い合わせ内容:\n$message\n";
+    $autoReplyBody .= "----------------------------\n\n";
+    $autoReplyBody .= "担当者より追ってご連絡いたしますので、今しばらくお待ちください。\n";
+    $autoReplyBody .= "よろしくお願いいたします。\n";
+
+    // 自動返信メールヘッダー
+    $autoReplyHeaders = "From: register@g-hill.jp\r\n";
+
+    // 自動返信メール送信
+    mail($email, $autoReplySubject, $autoReplyBody, $autoReplyHeaders);
+
+    // thanks.html へリダイレクト
+    header("Location: thanks.html");
+    exit;
+  } else {
+    echo "メールの送信に失敗しました。";
+  }
+}
+?>
